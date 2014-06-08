d6543bf96bb13e3bb4e4ef562457739f306206d3
Joshua Wood <stayupthetree@gmail.com>
2014-06-07 22:00:00 -0400

    Initial Commit

---
+++ PeopleDatabase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PeopleDatabase
+//
+//  Created by Joshua Wood on 6/7/14.
+//  Copyright (c) 2014 Joshua Wood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
